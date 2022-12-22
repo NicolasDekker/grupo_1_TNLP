91783a32e6b67fcee19546285e8129069670f498
Jeronimo Renaldi <jeroslp@gmail.com>
2022-12-22 12:57:24 -0300

    Header terminado y responsive, se agregaron vistas y se modofico el app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const path= require('path')
+
 app.use('/static', express.static(__dirname + '/public'));
 app.use(express.static('public'));
 
@@ -8,8 +10,23 @@ app.listen(3000, () => {
     console.log('Servidor corriendo puerto 3000')
 })
 
-
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve('./views/login.html'))
+})
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve('./views/register.html'))
+})
+
+app.get('/equipos', (req,res) => {
+    res.sendFile(path.resolve('./views/login.html'))
+})
+
+app.get('/carrito', (req,res) => {
+    res.sendFile(path.resolve('./views/carrito.html'))
 })
 
