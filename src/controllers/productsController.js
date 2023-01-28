74975c11283a2d161759aea0adbd4e8c978a547f
Jeronimo Renaldi <jeroslp@gmail.com>
2023-01-28 15:43:41 -0300

    Funcionalidades de crear y eliminar productos y cambios con EJS en las vistas de create y list

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,17 +4,41 @@ const productsJSON = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsJSON, 'utf-8'))
 
 const controllerProduct = {
-   
+
     list: (req, res) => { // Método para renderizar el listado de productos
         return res.render("products/list", {products: products});
     },
 
+    createProcess: (req, res) =>{  // Método para crear un producto
+        let id = products[products.length-1].id + 1;
+        console.log(req.body)
+        let productoNuevo = {id, ...req.body}
+        console.log(productoNuevo)
+        products.push(productoNuevo);
+        fs.writeFileSync(productsJSON, JSON.stringify(products, null, 2))
+        return res.redirect('/products')
+    },
+
+    create: (req,res) => {
+        res.render("products/create", {products: products});
+    },
+
+    edit: (req,res) =>{
+        res.render("products/edit");
+    },
+
     carrito: (req,res) => {
-        res.render("products/carrito");
+        return res.render("products/carrito");
     },
 
     detailProduct: (req,res) => {
-        res.render("products/detailProduct");
+        return res.render("products/detailProduct");
+    },
+
+    delete: (req, res) => {
+        let productFiltrados = products.filter(product => product.id != req.params.id)
+        fs.writeFileSync(productsJSON, JSON.stringify(productFiltrados, null, 2))
+        return res.render('products/list', {products: productFiltrados})
     }
 }
 
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -7,6 +7,14 @@ productsRouter.get('/carrito', productsController.carrito);
 
 productsRouter.get('/', productsController.list);
 
+productsRouter.post('/create', productsController.createProcess);
+
+productsRouter.get('/create', productsController.create);
+
+productsRouter.delete('/delete/:id', productsController.delete)
+
+productsRouter.get('/edit', productsController.edit );
+
 productsRouter.get('/detailProduct', productsController.detailProduct);
 
 module.exports = productsRouter;
